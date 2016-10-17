bc670ee3bf0a13a76ecad48c38f7fe7bab739b34
JasonHJWQ <jiawei.han@qq.com>
2016-10-17 12:58:11 +0800

    delete comments

--- src/org/hh/aibot/aibot.java
+++ src/org/hh/aibot/aibot.java
@@ -3,7 +3,7 @@ package org.hh.aibot;
 public class aibot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("Hello World!");
 		System.out.println("Git test bot file!");
 		for(int i=1;i<20;i++){
