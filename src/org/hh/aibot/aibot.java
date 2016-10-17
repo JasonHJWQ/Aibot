e3a7f29ff5c03be19c6d2024db6a512920c21ef3
JasonHJWQ <jiawei.han@qq.com>
2016-10-17 12:51:28 +0800

    for test new

--- src/org/hh/aibot/aibot.java
+++ src/org/hh/aibot/aibot.java
@@ -6,6 +6,9 @@ public class aibot {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Git test bot file!");
+		for(int i=1;i<20;i++){
+			System.out.println("Ñ­»·²âÊÔ");
+		}
 	}
 
 }
